88ca417b034679f465fde8cf37882426a149a0e0
RenatoGhirelli <79719528+RenatoGhirelli@users.noreply.github.com>
2025-11-23 20:29:58 -0300

    adicionando serviço de histórico

---
+++ app-service-historico/src/main/java/com/techcallenge3/historicographql/HistoricographqlFiapFase3Application.java
@@ -0,0 +1,13 @@
+package com.techcallenge3.historicographql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricographqlFiapFase3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoricographqlFiapFase3Application.class, args);
+	}
+
+}
---
+++ app-service-historico/src/test/java/com/techcallenge3/historicographql/HistoricographqlFiapFase3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcallenge3.historicographql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoricographqlFiapFase3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
